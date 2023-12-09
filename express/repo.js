b38a81ca7733847c3a39b16c14e54773ca626b19
einsy-dev <eins.yeager@gmail.com>
2023-12-09 18:16:28 +0600

    abstract class init

--- express/repo.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-class BooksRepository {
-}
-module.exports = BooksRepository;
--- express/repo.ts
+++ express/repo.ts
@@ -10,11 +10,26 @@ interface Book {
 }
 
 abstract class BooksRepository {
-  abstract getBooks(): Promise<Book[]>;
-  abstract getBook(id: string): Promise<Book>;
-  abstract createBook(book: Book): Promise<void>;
-  abstract updateBook(book: Book): Promise<void>;
-  abstract deleteBook(id: string): Promise<void>;
+  getBooks(): void {
+    console.log("Abstract class method");
+  }
+  getBook(id: string): void {
+    throw new Error("Method not implemented." + id);
+  }
+  abstract createBook(book: Book): void;
+  abstract updateBook(book: Book): void;
+  abstract deleteBook(id: string): void;
 }
 
-module.exports = BooksRepository;
+class Repo extends BooksRepository {
+  createBook(book: Book): void {
+    throw new Error("Method not implemented." + book);
+  }
+  updateBook(book: Book): void {
+    throw new Error("Method not implemented." + book);
+  }
+  deleteBook(id: string): void {
+    throw new Error("Method not implemented." + id);
+  }
+}
+module.exports = Repo
